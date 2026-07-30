704d14ffe3b06729910f56482b6fea650d91569c
Gabriel Goranov <gabi.goranov.work@gmail.com>
2026-07-30 22:48:19 +0300

    Read from console

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,22 @@
 module Lib
-    ( someFunc
+    ( mainFunc
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Codec.Picture
+
+mainFunc :: IO ()
+mainFunc = do
+    putStrLn "Enter image path:"
+    path <- getLine
+
+    putStrLn "Enter pixel at X:"
+    pixelXPosInput <- getLine
+    let pixelXPos = read pixelXPosInput :: Int
+
+    putStrLn "Enter pixel at Y:"
+    pixelYPosInput <- getLine
+    let pixelYPos = read pixelYPosInput :: Int
+
+    let image = readImage path
+    
+    putStrLn (show pixelXPos)
